4d0afa121a5d1e690f7adebb68aa0971cd7f3536
tarasovalex <tarasov.alex@gmail.com>
2018-02-20 22:47:49 +0200

    Spring cloud config server

---
+++ cloud-config-demo/cloude-config-server/src/main/java/com/cloude/demo/cloudeconfig/server/CloudeConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.cloude.demo.cloudeconfig.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudeConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudeConfigServerApplication.class, args);
+	}
+}
